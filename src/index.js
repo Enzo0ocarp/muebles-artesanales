d245a3a5f8072dfcd390495919ddf7b9b9db715f
Enzo0ocarp <cartaspoke3@gmail.com>
2025-06-23 19:43:48 -0300

    index corregido

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+/**
+ * @fileoverview Punto de entrada principal de la aplicación React
+ * @author Muebles Artesanales
+ * @version 1.0.0
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * Punto de entrada principal de la aplicación
+ * Renderiza el componente App en el DOM
+ * 
+ * @function
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/**
+ * Función para medir el rendimiento de la aplicación
+ * Si se pasa una función, registra los resultados (ej: reportWebVitals(console.log))
+ * o envía a un endpoint de analytics. Más info: https://bit.ly/CRA-vitals
+ */
 reportWebVitals();
